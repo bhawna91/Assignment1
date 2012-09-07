3b145fea1e6251c629e36987a5c92e1c7ef2a7b2
bhawna <bhawna.faujdar@gmail.com>
2012-09-07 07:10:33 +0530

    Formatted code

--- code.c
+++ code.c
@@ -54,7 +54,7 @@ int min_val(int* x,int n) {         /*returns the minclicks value among all the 
 }
 
 
-void get_data() {
+void get_data() {                   /*Get the user data*/
  char low_high[10];
  char blocked_list[210];
  char channel_list[260];
@@ -63,7 +63,7 @@ void get_data() {
  gets(low_high);
  gets(blocked_list);
  gets(channel_list);
- while(low_high[i]!='\0'){                   /*storing the min and max channel value */
+ while(low_high[i]!='\0'){          /*storing the min and max channel value */
   if(low_high[i]==32)
       j++;
   else if(j==0)
@@ -73,7 +73,7 @@ void get_data() {
   i++;
  }
  i=j=0;
- while(blocked_list[i]!='\0'){               /*for storing the list of blocked channels*/
+ while(blocked_list[i]!='\0'){      /*for storing the list of blocked channels*/
      if(blocked_list[i]==32)
        bnum++;
      else
@@ -81,7 +81,7 @@ void get_data() {
      i++;
  }
  i=0;
- while(channel_list[i]!='\0'){              /*for storing the channels to be viewed*/
+ while(channel_list[i]!='\0'){      /*for storing the channels to be viewed*/
      if(channel_list[i]==32)
        cnum++;
      else
@@ -91,7 +91,7 @@ void get_data() {
 }
 
 
-void channel_up(int i,int index) {
+void channel_up(int i,int index) {        
      int j;
      while(curr_ch!=clist[i]){
          if(curr_ch==high)
@@ -135,45 +135,45 @@ void channel_down(int i,int index) {
 
 int min_clicks() {
     int i;
-    mclicks+=get_digits(clist[0]);  //for the first channel, you will always key in the digits
+    mclicks+=get_digits(clist[0]);     /*for the first channel, you will always key in the digits*/
     curr_ch=clist[0];
-    for(i=1;i<=cnum;i++){  //run loop from second channel to cnum
-      init(clicks,8);  //initialize clicks to 0 at the beginning of every iteration
+    for(i=1;i<=cnum;i++){               /*run loop from second channel to cnum*/
+      init(clicks,8);                   /*initialize clicks to 0 at the beginning of every iteration*/
 
       clicks[0]=get_digits(clist[i]);
-      //end of part 1 - directly key in the digits
+      /*end of part 1 - directly key in the digits*/
 
       curr_ch=clist[i-1];
       channel_up(i,1);
-      //end of part 2 - keep pressing up
+      /*end of part 2 - keep pressing up*/
 
       curr_ch=clist[i-1];
       channel_down(i,2);
-      //end of part 3 - keep pressing down
+      /*end of part 3 - keep pressing down*/
 
       curr_ch=low;
       clicks[3]+=get_digits(low);
       channel_down(i,3);
-      //end of part 4 - key in min value and go back
+      /*end of part 4 - key in min value and go back*/
 
       curr_ch=high;
       clicks[4]+=get_digits(high);
       channel_up(i,4);
-      //end of part 5 - key in max value and go forward
+      /*end of part 5 - key in max value and go forward*/
 
      if(i>1){
       curr_ch=clist[i-2];
       clicks[5]++;
       channel_up(i,5);
      } else clicks[5]=10000;
-      //end of part 6 - press back and then keep pressing down
+      /*end of part 6 - press back and then keep pressing down*/
 
      if(i>1) {
       curr_ch=clist[i-2];
       clicks[6]++;
       channel_down(i,6);
      } else clicks[6]=10000;
-      //end of part 7 - press back and then keep pressing up
+      /*end of part 7 - press back and then keep pressing up*/
 
       mclicks+=min_val(clicks,7); /*increment minclicks by the min value among the procedures followed for current channel*/
     }
